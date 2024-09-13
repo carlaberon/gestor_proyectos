5d6ae3db87a9ad74a873979a8c4ece69b54f6c78
Carla Berón <alracnoreb@gmail.com>
2024-09-13 18:45:13 -0300

    Update CrearProyecto.java
    ventana crear  nuevo proyecto

--- FRONTEND/src/ar/edu/unrn/seminario/gui/CrearProyecto.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/CrearProyecto.java
@@ -20,6 +20,9 @@ import javax.swing.JTextPane;
 import javax.swing.JTree;
 import java.awt.Font;
 import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
 
 public class CrearProyecto extends JFrame {
 
@@ -40,31 +43,38 @@ public class CrearProyecto extends JFrame {
 	*/
 		setTitle("");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 650);
 		getContentPane().setLayout(null);
 		
 		contentPane = new JPanel();
+		contentPane.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		contentPane.setBackground(new Color(81, 79, 89));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
 		setContentPane(contentPane);
 
-		JLabel nombreProyecto = new JLabel("Nombre Proyecto:");
-		nombreProyecto.setFont(new Font("Gill Sans MT", Font.PLAIN, 10));
-		nombreProyecto.setBounds(64, 76, 113, 16);
+		JLabel nombreProyecto = new JLabel("Nombre Proyecto*:");
+		nombreProyecto.setForeground(new Color(240, 240, 240));
+		nombreProyecto.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		nombreProyecto.setBounds(88, 145, 227, 39);
 		contentPane.add(nombreProyecto);
 
 		
 
 		nombreProyectoTextField = new JTextField();
-		nombreProyectoTextField.setBounds(187, 74, 160, 22);
+		nombreProyectoTextField.setBounds(325, 157, 390, 25);
 		contentPane.add(nombreProyectoTextField);
 		nombreProyectoTextField.setColumns(10);
 
 
 		JButton aceptarButton = new JButton("Guardar");
-		aceptarButton.setForeground(new Color(255, 255, 255));
-		aceptarButton.setBackground(new Color(10, 118, 58));
-		aceptarButton.setFont(new Font("Gill Sans MT", Font.PLAIN, 10));
+		aceptarButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		aceptarButton.setForeground(new Color(229, 212, 237));
+		aceptarButton.setBackground(new Color(89, 65, 169));
+		aceptarButton.setFont(new Font("Segoe UI", Font.BOLD, 14));
 		/*
 		aceptarButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -79,38 +89,54 @@ public class CrearProyecto extends JFrame {
 			}
 		}); 
 		*/
-		aceptarButton.setBounds(218, 215, 97, 25);
+		aceptarButton.setBounds(395, 398, 147, 27);
 		contentPane.add(aceptarButton);
 
 		JButton cancelarButton = new JButton("Cancelar");
-		cancelarButton.setBackground(new Color(238, 238, 238));
-		cancelarButton.setFont(new Font("Gill Sans MT", Font.PLAIN, 10));
+		cancelarButton.setForeground(new Color(29, 17, 40));
+		cancelarButton.setBackground(new Color(229, 212, 237));
+		cancelarButton.setFont(new Font("Segoe UI", Font.BOLD, 14));
 		cancelarButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				setVisible(false);
 				dispose();
 			}
 		});
-		cancelarButton.setBounds(323, 215, 97, 25);
+		cancelarButton.setBounds(568, 398, 147, 27);
 		contentPane.add(cancelarButton);
 
 
 
-		JLabel rolLabel = new JLabel("Subproyecto:");
-		rolLabel.setFont(new Font("Gill Sans MT", Font.PLAIN, 10));
-		rolLabel.setBounds(64, 129, 97, 16);
+		JLabel rolLabel = new JLabel("Subproyecto de:");
+		rolLabel.setForeground(new Color(240, 240, 240));
+		rolLabel.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		rolLabel.setBounds(88, 233, 167, 39);
 		contentPane.add(rolLabel);
 
 
 
 		proyectoComboBox = new JComboBox();
-		proyectoComboBox.setBounds(187, 126, 160, 22);
+		proyectoComboBox.setForeground(new Color(29, 17, 40));
+		proyectoComboBox.setFont(new Font("Segoe UI", Font.PLAIN, 10));
+		proyectoComboBox.setBounds(325, 245, 390, 25);
 		contentPane.add(proyectoComboBox);
 		
 		JLabel lblNewLabel = new JLabel("Nuevo Proyecto");
-		lblNewLabel.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
-		lblNewLabel.setBounds(20, 22, 192, 31);
+		lblNewLabel.setForeground(new Color(29, 17, 40));
+		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 35));
+		lblNewLabel.setBounds(35, 47, 291, 73);
 		contentPane.add(lblNewLabel);
+		
+		JLabel lblConfiguracinAvanzada = new JLabel("Configuración avanzada:");
+		lblConfiguracinAvanzada.setForeground(UIManager.getColor("Button.background"));
+		lblConfiguracinAvanzada.setFont(new Font("Segoe UI", Font.PLAIN, 15));
+		lblConfiguracinAvanzada.setBounds(509, 316, 167, 39);
+		contentPane.add(lblConfiguracinAvanzada);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("");
+		chckbxNewCheckBox.setBackground(new Color(81, 79, 89));
+		chckbxNewCheckBox.setBounds(694, 323, 21, 32);
+		contentPane.add(chckbxNewCheckBox);
 
 		/*
 		for (ProyectoDTO proyect : this.proyectos) {
