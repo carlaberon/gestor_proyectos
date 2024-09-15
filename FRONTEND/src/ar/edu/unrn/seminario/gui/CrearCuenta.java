c90cb5b30f0e5294bbe1ad86c17a75b9a9031b8a
Carla Berón <alracnoreb@gmail.com>
2024-09-15 13:29:54 -0300

    Update CrearCuenta.java
    acomodé la fuente, elimine algunos campos que no estaban especificados en el usuario que definimos y acomode los botones

--- FRONTEND/src/ar/edu/unrn/seminario/gui/CrearCuenta.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/CrearCuenta.java
@@ -28,18 +28,10 @@ public class CrearCuenta extends JFrame {
 	private JTextField textField_2;
 	private JLabel lblNewLabel_1_2;
 	private JTextField textField_3;
-	private JLabel lblNewLabel_1_3;
-	private JTextField textField_4;
 	private JButton aceptar;
 	private JButton cancelar;
-	private JLabel lblNewLabel_1_4;
-	private JTextField textField_5;
 	private JLabel lblNewLabel_1_5;
 	private JTextField textField_6;
-	private JLabel lblNewLabel_2;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_1_7;
-	private JLabel lblNewLabel_1_6;
 
 	/**
 	 * Launch the application.
@@ -76,69 +68,58 @@ public class CrearCuenta extends JFrame {
 		lblNewLabel.setBackground(new Color(240, 240, 240));
 		lblNewLabel.setForeground(new Color(29, 17, 40));
 		lblNewLabel.setFont(new Font("Segoe UI", Font.PLAIN, 35));
-		lblNewLabel.setBounds(276, 30, 234, 39);
+		lblNewLabel.setBounds(58, 47, 234, 39);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Usuario*:");
 		lblNewLabel_1.setForeground(new Color(255, 255, 255));
-		lblNewLabel_1.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1.setBounds(241, 105, 93, 44);
+		lblNewLabel_1.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(83, 119, 93, 44);
 		contentPane.add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(314, 116, 172, 26);
+		textField.setBounds(216, 134, 451, 26);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Nombre*:");
 		lblNewLabel_1_1.setForeground(Color.WHITE);
-		lblNewLabel_1_1.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_1.setBounds(138, 150, 93, 44);
+		lblNewLabel_1_1.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		lblNewLabel_1_1.setBounds(83, 174, 93, 44);
 		contentPane.add(lblNewLabel_1_1);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(219, 162, 172, 26);
+		textField_1.setBounds(216, 189, 451, 26);
 		contentPane.add(textField_1);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Apellido*:");
 		lblNewLabel_1_1_1.setForeground(Color.WHITE);
-		lblNewLabel_1_1_1.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_1_1.setBounds(413, 149, 93, 44);
+		lblNewLabel_1_1_1.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		lblNewLabel_1_1_1.setBounds(83, 228, 93, 44);
 		contentPane.add(lblNewLabel_1_1_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(491, 161, 172, 26);
+		textField_2.setBounds(216, 243, 451, 26);
 		contentPane.add(textField_2);
 		
 		lblNewLabel_1_2 = new JLabel("Email*:");
 		lblNewLabel_1_2.setForeground(Color.WHITE);
-		lblNewLabel_1_2.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_2.setBounds(138, 206, 66, 44);
+		lblNewLabel_1_2.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		lblNewLabel_1_2.setBounds(83, 280, 93, 44);
 		contentPane.add(lblNewLabel_1_2);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-		textField_3.setBounds(219, 217, 370, 26);
+		textField_3.setBounds(216, 295, 451, 26);
 		contentPane.add(textField_3);
 		
-		lblNewLabel_1_3 = new JLabel("Domicilio:");
-		lblNewLabel_1_3.setForeground(Color.WHITE);
-		lblNewLabel_1_3.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_3.setBounds(138, 410, 93, 44);
-		contentPane.add(lblNewLabel_1_3);
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(219, 422, 370, 26);
-		contentPane.add(textField_4);
-		
 		aceptar = new JButton("Aceptar");
-		aceptar.setFont(new Font("Segoe UI", Font.BOLD, 13));
+		aceptar.setFont(new Font("Segoe UI", Font.BOLD, 14));
 		aceptar.setForeground(new Color(255, 255, 255));
 		aceptar.setBackground(new Color(89, 65, 169));
-		aceptar.setBounds(534, 574, 111, 26);
+		aceptar.setBounds(342, 452, 147, 27);
 		aceptar.addActionListener(new ActionListener() {
 			
 			@Override
@@ -151,86 +132,21 @@ public class CrearCuenta extends JFrame {
 		contentPane.add(aceptar);
 		
 		cancelar = new JButton("Cancelar");
-		cancelar.setFont(new Font("Segoe UI", Font.BOLD, 13));
-		cancelar.setForeground(new Color(255, 255, 255));
-		cancelar.setBackground(new Color(89, 65, 169));
-		cancelar.setBounds(663, 574, 111, 26);
+		cancelar.setFont(new Font("Segoe UI", Font.BOLD, 14));
+		cancelar.setForeground(new Color(29, 17, 40));
+		cancelar.setBackground(new Color(229, 212, 237));
+		cancelar.setBounds(520, 452, 147, 27);
 		contentPane.add(cancelar);
-		
-		lblNewLabel_1_4 = new JLabel("Teléfono:");
-		lblNewLabel_1_4.setForeground(Color.WHITE);
-		lblNewLabel_1_4.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_4.setBounds(138, 465, 87, 44);
-		contentPane.add(lblNewLabel_1_4);
-		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(219, 477, 172, 26);
-		contentPane.add(textField_5);
 		
 		lblNewLabel_1_5 = new JLabel("Contraseña*:");
 		lblNewLabel_1_5.setForeground(Color.WHITE);
-		lblNewLabel_1_5.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_5.setBounds(138, 261, 120, 44);
+		lblNewLabel_1_5.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+		lblNewLabel_1_5.setBounds(83, 334, 120, 44);
 		contentPane.add(lblNewLabel_1_5);
 		
 		textField_6 = new JTextField();
 		textField_6.setColumns(10);
-		textField_6.setBounds(237, 272, 210, 26);
+		textField_6.setBounds(216, 349, 451, 26);
 		contentPane.add(textField_6);
-		
-		lblNewLabel_2 = new JLabel("(opcional)");
-		lblNewLabel_2.setFont(new Font("Segoe UI", Font.BOLD, 12));
-		lblNewLabel_2.setBounds(597, 428, 66, 14);
-		contentPane.add(lblNewLabel_2);
-		
-		lblNewLabel_3 = new JLabel("(opcional)");
-		lblNewLabel_3.setFont(new Font("Segoe UI", Font.BOLD, 12));
-		lblNewLabel_3.setBounds(400, 483, 76, 14);
-		contentPane.add(lblNewLabel_3);
-		
-		lblNewLabel_1_7 = new JLabel("Fecha de Nacimiento*");
-		lblNewLabel_1_7.setForeground(Color.WHITE);
-		lblNewLabel_1_7.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_7.setBounds(138, 307, 210, 44);
-		contentPane.add(lblNewLabel_1_7);
-		
-		JComboBox comboBoxDia = new JComboBox();
-		comboBoxDia.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		comboBoxDia.setBounds(138, 354, 93, 26);
-		contentPane.add(comboBoxDia);
-		comboBoxDia.addItem("Día");
-		
-		JComboBox comboBoxMes = new JComboBox();
-		comboBoxMes.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		comboBoxMes.setBounds(241, 354, 93, 26);
-		contentPane.add(comboBoxMes);
-		comboBoxMes.addItem("Mes");
-		
-		JComboBox comboBoxAño = new JComboBox();
-		comboBoxAño.setFont(new Font("Segoe UI", Font.PLAIN, 11));
-		comboBoxAño.setBounds(344, 354, 93, 26);
-		contentPane.add(comboBoxAño);
-		comboBoxAño.addItem("Año");
-		
-		lblNewLabel_1_6 = new JLabel("Género*");
-		lblNewLabel_1_6.setForeground(Color.WHITE);
-		lblNewLabel_1_6.setFont(new Font("Segoe UI", Font.BOLD, 14));
-		lblNewLabel_1_6.setBounds(473, 307, 93, 44);
-		contentPane.add(lblNewLabel_1_6);
-		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Masculino");
-		rdbtnNewRadioButton.setForeground(new Color(255, 255, 255));
-		rdbtnNewRadioButton.setFont(new Font("Segoe UI", Font.BOLD, 11));
-		rdbtnNewRadioButton.setBackground(new Color(81, 79, 89));
-		rdbtnNewRadioButton.setBounds(467, 355, 87, 23);
-		contentPane.add(rdbtnNewRadioButton);
-		
-		JRadioButton rdbtnFemenino = new JRadioButton("Femenino");
-		rdbtnFemenino.setForeground(Color.WHITE);
-		rdbtnFemenino.setFont(new Font("Segoe UI", Font.BOLD, 11));
-		rdbtnFemenino.setBackground(new Color(81, 79, 89));
-		rdbtnFemenino.setBounds(556, 355, 97, 23);
-		contentPane.add(rdbtnFemenino);
 	}
 }
