e8b0a0b4fbf9faa2ffc07bda81f9d2ac987ebcbd
EzeRamos17 <hernan_ram99@hotmail.com>
2024-10-09 11:17:48 -0300

    Modificacion ListaProyectos
    hice unas modificaciones en la lista de proyectos para que se vean bien cuando se crean y los que esten inicializados

--- FRONTEND/src/ar/edu/unrn/seminario/gui/Inicio.java
+++ FRONTEND/src/ar/edu/unrn/seminario/gui/Inicio.java
@@ -177,7 +177,7 @@ public class Inicio extends JFrame {
     }
 
     private void abrirListaProyectos() {
-        ListaProyectos listaProyectos = new ListaProyectos(); // Crear una instancia de ListaProyectos
+        ListaProyectos listaProyectos = new ListaProyectos(api); // Crear una instancia de ListaProyectos
         listaProyectos.setVisible(true); // Hacer visible la ventana de proyectos
         //frame.dispose(); // Cerrar la ventana actual
         
